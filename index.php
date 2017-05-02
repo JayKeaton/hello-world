d3e036bcd6aa73345fdf8637b90ad7e3d12dcbaf
mirai 260 <mirai 260>
2017-05-02 20:00:22 +0200

    website_bases

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$page = $_GET['page'];
+
+
+
+
+if (empty($page)){
+	header("Location: /?page=accueil");
+}
+elseif ($page == "accueil"){
+	include("templates/accueil.html");
+}
+elseif ($page == "dascapital"){
+	include("templates/dascapital.html");
+}
+elseif ($page == "sign_up"){
+    include("templates/sign_up.html");
+}
+else{
+	header("Location: /?page=accueil");
+}
