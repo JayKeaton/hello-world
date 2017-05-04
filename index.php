c6a0c13f7e0bfb5f2cd357707460dce6347f65fd
hugobriet <hugo.briet@isep.fr>
2017-05-04 22:15:08 +0200

    index mis à jour

--- index.php
+++ index.php
@@ -1,5 +1,22 @@
-<?php 
+<?php
 
 include("models/SQLCo.php");
 include("models/utilisateur.php");
-include("controllers/signup.php");
+
+
+
+if (!empty($_GET['page']))
+    $page = $_GET['page'];
+
+
+
+
+if (empty($page)){
+    include("templates/accueil.html");
+}
+elseif ($page == "signup"){
+    include("controllers/signup.php");
+}
+else{
+    include("templates/".$page.".html");
+}
