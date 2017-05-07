dbe78fb7e13f9eea44f66750e3411fd14d0104c9
hugobriet <hugo.briet@isep.fr>
2017-05-07 15:04:32 +0200

    ajout .htaccess

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 
+
+$root =
+    (!empty($_SERVER['HTTPS']) ? 'https' : 'http'). // https ?
+    '://'.
+    $_SERVER['HTTP_HOST'].  // Adresse du serveur
+    $_SERVER['REQUEST_URI'];    // Chemin relatif du dossier courant sur le serveur
+
+define('CHEMIN_SITE', $root);
+
+
 include("models/SQLCo.php");
 include("models/utilisateur.php");
-
 
 
 if (!empty($_GET['page']))
