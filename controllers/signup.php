c2206f53d065d4367be72a63230d6666ee237f6c
Hugo Briet <hugo.briet@isep.fr>
2017-05-08 21:28:52 +0200

    Update signup.php

--- controllers/signup.php
+++ controllers/signup.php
@@ -22,6 +22,16 @@
 		 $root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
 		 echo "Erreur, vous êtes déjà inscrit. Connectez vous : " .$root."?page=login";
 		 }
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
 	elseif($_POST["mdp"] != $_POST["mdpv"]){
 		echo "Mot de passe différent de la confirmation";
 		include("templates/Signup.php");
