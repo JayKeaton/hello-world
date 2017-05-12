dc6bc39d1730c437b45ed60e14d64b00cd803c1f
mirai260 <hugo.briet@isep.fr>
2017-05-12 14:03:41 +0200

    modif bdd

--- bdd.sql
+++ bdd.sql
@@ -2,33 +2,44 @@
 
 
 --
--- Base de données: `APP-MVC`
+-- Base de données: `error404`
 --
 CREATE DATABASE `error404` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 USE `error404`;
 
 
--- --------------------------------------------------------
 
---
--- Structure de la table `Utilisateurs`
---
+
+CREATE TABLE Categories(
+  idCategorie INTEGER PRIMARY KEY AUTO_INCREMENT,
+  code VARCHAR(255),
+  langue VARCHAR(255),
+  traduction VARCHAR(255)
+);
 
 
 
 CREATE TABLE Services (
   idService INTEGER PRIMARY KEY AUTO_INCREMENT,
   validation BOOLEAN DEFAULT false,
-  localisation VARCHAR(255),
+  codePostal INTEGER,
+  ville VARCHAR(255),
+  rue VARCHAR(255),
+  numéro INTEGER,
   categorie VARCHAR(255),
-  telephone INTEGER(20),
+  telephone VARCHAR(20),
   mail VARCHAR(255),
   lien_site VARCHAR(255),
   idContributeur INTEGER
     REFERENCES Utilisateurs(idUtilisateur) ON DELETE SET NULL
 );
 
---CREATE TABLE Seances
+CREATE TABLE Seances(
+  idSeance INTEGER PRIMARY KEY AUTO_INCREMENT,
+  date DATE,
+  idService INTEGER
+    REFERENCES Services(idService)
+);
 
 
 
@@ -52,6 +63,7 @@ CREATE TABLE Utilisateurs (
   avatar VARCHAR(255),
   nom VARCHAR(100),
   prenom VARCHAR(100),
+  dateNaissance DATE,
   mail VARCHAR(255),
   verification BOOLEAN DEFAULT false,
   cle VARCHAR(255),
@@ -84,5 +96,8 @@ CREATE TABLE Commentaires(
   idUtilisateur INTEGER
     REFERENCES Utilisateurs(idUtilisateur) ON DELETE CASCADE,
   idService INTEGER NOT NULL
-    REFERENCES Services(idService) ON DELETE CASCADE
+    REFERENCES Services(idService) ON DELETE CASCADE,
+  idSeance INTEGER NOT NULL
+    REFERENCES Seance(idSeance) ON DELETE CASCADE,
+
 );
