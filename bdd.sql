aea6df2d74d097ba7d2d1df3743077cdf377b71d
legyllith <jeremy.n.49@live.fr>
2017-05-18 10:20:14 +0200

    modification bdd

--- bdd.sql
+++ bdd.sql
@@ -67,7 +67,9 @@ CREATE TABLE utilisateurs (
   dateNaissance DATE,
   verification BOOLEAN DEFAULT false,
   cle VARCHAR(255),
+  numero VARCHAR(255),
   adresse VARCHAR(255),
+  ville VARCHAR(255),
   droits VARCHAR(20),
   telephone VARCHAR(20)
 );
@@ -100,3 +102,10 @@ CREATE TABLE commentaires(
   idSeance INTEGER NOT NULL
     REFERENCES seances(idSeance) ON DELETE CASCADE
 );
+
+INSERT INTO `services`(`validation`, `codePostal`, `ville`, `rue`, `numéro`, `categorie`, `telephone`, `mail`, `lien_site`) VALUES (0,75000,"Paris","rue de Rivoli",5,"accompagnement médical","0625523251","0001@0001","http://www.dofus.com/fr");
+INSERT INTO `services`(`validation`, `codePostal`, `ville`, `rue`, `numéro`, `categorie`, `telephone`, `mail`, `lien_site`) VALUES (0,75000,"Paris","Rue Vieille du Temple",21,"logement","0658921542","0002@0002","http://euw.leagueoflegends.com/fr");
+INSERT INTO `utilisateurs`( `pseudo`, `mail`, `mdp`, `avatar`, `nom`, `prenom`, `dateNaissance`, `verification`, `numero`,`adresse`,`ville`, `droits`, `telephone`) VALUES ("jean eude","jean.eude@kikoolol.fr","saphir","avatar-j-e.jpg","jean-eude","debeaujardin","1982-06-02",1,"22","rue Vieille du Temple","Paris","utilisateur","0645895121");
+INSERT INTO `utilisateurs`( `pseudo`, `mail`, `mdp`, `avatar`, `nom`, `prenom`, `dateNaissance`, `verification`, `numero`,`adresse`,`ville`, `droits`, `telephone`) VALUES ("legyllith","dieu.de.la.bonte@divinité.ciel","gentil","avatar-legyllith.jpg","Aurélien","dreams","1992-06-12",1,"100","rue des archives","Paris","contributeur","0645884521");
+INSERT INTO `descriptions`(`nom`, `texte`, `langue`) VALUES ("soin +","Nous serons heureux de vous soigné","francais");
+INSERT INTO `descriptions`( `nom`, `texte`, `langue`) VALUES ("acceillir","Nous vous acciullerons avec plaisir","francais");
