254318b29610cadcce83cdb7f38ed2e3440c2b45
Vincent-Isep <vincent.idrissov@isep.fr>
2017-05-19 16:58:54 +0200

    CSS<3

--- controllers/accueil.php
+++ controllers/accueil.php
@@ -1,4 +1,16 @@
 <?php
+if(!empty($_POST['catégorie'])){
+		$typeService = $_POST['catégorie'];
+
+		$req=$bdd->prepare("SELECT * FROM services WHERE idUtilisateur=idUtilisateur");
+	$req->execute(array('idUtilisateur'=>$_SESSION['idUtilisateur']));
+	$data=$req->fetchall();
+		
+	$req = $bdd->prepare("SELECT * FROM services WHERE categorie=$categorie ORDER BY distance");
+	$req->execute(array('idUtilisateur'=>$_SESSION['idUtilisateur']));
+	$data=$req->fetchall();
+	}
 include ("templates/accueil.php");
+
 ?>
 
--- templates/accueil.php
+++ templates/accueil.php
@@ -28,84 +28,98 @@
 					<a href="https://fr.wikipedia.org/wiki/Param%C3%A8tre">
 						<li><div><img src="static/accueil/msf_logo _fichiers/engrenages_1.png"width=20><br/>Paramètres</div></li></a>			
 					
-
 			    </ul>
    			</header>
 
 			<h1 id="myTitle">Pour <strong>VOUS</strong></h1>
 			<h3 id="LittleTitle">Par le groupe Error 404</h3>
 			<p id="description">MSF a été créé pour contribuer à la protection de la vie et à l'allégement des souffrances en respect de la dignité humaine. MSF apporte des soins à des personnes en situation précaire et travaille à leur permettre de reprendre le contrôle sur leur futur.<br/></p>
-			<section id="articles">
-				<div id="presentation">
-					<h2>Fonctionnalités:</h2>
-					<p>
-					-services classés par catégories: onglets où dormir, où manger, où se faire soigner, où trouver une aide juridique, des vêtements, des services administratifs dédiés, etc. <br/>
-					- services classés par proximité géographique<br/>
-					-Affichage des services en liste, ou par carte
-					-Langues: français,anglais, arabe, farsi + API traduction Google<br/>
-					-Géolocalisation pour repérer les services d’aide lien vers Maps, Citymapper, etc.<br/>
-					-Mode hors connexion<br/>
-					-Partage via réseaux sociaux, etc.<br/>
-					-Interface contributeur(particulier, ONG) avec login/mot de passe: permet d’ajouter un service à proposer<br/>
-					Interface super admin avec login/mot de passe pour vérifier l'authenticité des informations saisies.<br/>
-					-Le projet est fait en Open Source<br/></p>
-				</div>
-				<div id = "recherche">
-					<h1>Recherche de services</h1>
-			    	<form action="" method="POST" id="form">
-			    		<div class="element">
-			    			<p>Que recherchez-vous ?</p>
-				    		<select name="categorie" id="categorie">
-				    			<option name="soins">Soins</option>
-				    			<option name="nourriture">Nourriture</option>
-				    			<option name="logement">Logement</option>
-				    			<option name="aide_juridique">Aide juridique</option>
-				    			<option name="services_administratifs">Services administratifs</option>
-				    			<option name="vetements">Vêtements</option>
-				    		</select>
-				    		<p>   <a href="https://www.google.fr/advanced_search">Recherche Avancée</a></p>
-			    		</div>
-			    		<div class="element">
-				   			<p>Quel lieu?</p>
-				   			<input type="text" name="adresse" id="adresse" /><br/>			   
-				   			<input type="checkbox">Geolocalisation</input>
-				   		</div>
-			    		<div class="element">
-			    			<input type="submit" value="Go!"/>
-			    		</div>
-			    	</form>
-			    </div>
-				<div id="contenu">
-					<p><a href="https://soutenir.msf.fr/b/mon-don?esv_source=Google&esv_medium=sea_brand&esv_campaign=W%2A%2AW00001&esv_term=msf&gclid=COKwx8uJvdMCFccp0wodkDMP4Q">
-							<div id="article 2" class="article">
-								Nom random 1 - 643m<br/>
-								<img src="static/accueil/msf_logo _fichiers/maison.jpeg" width=240 class="floatl" title="photo non contractuelle"/>
-									Peut-être votre futur logement?<br/>.........................<br/>
-					...<br/>...............................
-					..............................................<br/><br/><br/>
-						DESCRIPTION
-							</div>
-						</a>
-					</p>
-					<p><a href="http://www.lecrystalparis.com/menu.html#mix">
-							<div id="article 1" class="article">
-								<figure>
-									<img src="static/accueil/msf_logo _fichiers/Worlds-Largest-Pizza.jpg" width=240 class=floatl title="photo non contractuelle" />
-									<figcaption>Repas à partager en famille<br/>.........................<br/>
+				<div id="articles">
+					<div id="presentation">
+						<h2>Fonctionnalités:</h2>
+						<p>
+						-services classés par catégories: onglets où dormir, où manger, où se faire soigner, où trouver une aide juridique, des vêtements, des services administratifs dédiés, etc. <br/>
+						- services classés par proximité géographique<br/>
+						-Affichage des services en liste, ou par carte
+						-Langues: français,anglais, arabe, farsi + API traduction Google<br/>
+						-Géolocalisation pour repérer les services d’aide lien vers Maps, Citymapper, etc.<br/>
+						-Mode hors connexion<br/>
+						-Partage via réseaux sociaux, etc.<br/>
+						-Interface contributeur(particulier, ONG) avec login/mot de passe: permet d’ajouter un service à proposer<br/>
+						Interface super admin avec login/mot de passe pour vérifier l'authenticité des informations saisies.<br/>
+						-Le projet est fait en Open Source<br/></p>
+					</div>
+					<div id = "recherche">
+						<h1>Recherche de services</h1>
+				    	<form action="" method="POST" id="form">
+				    		<div class="element">
+				    			<p>Que recherchez-vous ?</p>
+					    		<select name="categorie" id="categorie">
+					    			<option name="soins">Soins</option>
+					    			<option name="nourriture">Nourriture</option>
+					    			<option name="logement">Logement</option>
+					    			<option name="aide_juridique">Aide juridique</option>
+					    			<option name="services_administratifs">Services administratifs</option>
+					    			<option name="vetements">Vêtements</option>
+					    		</select>
+					    		<a href="https://www.google.fr/advanced_search"><div class="element">
+				    			<input type="submit" value="Recherche avancée"/>
+				    			</a>
+				    		</div>
+				    		<div class="element">
+					   			<!--<p>Quel lieu?</p>
+					   			<input type="text" name="adresse" id="adresse" /><br/>	-->
+					   			<div>
+							        <h4>Où chercher?</h4> 
+							        <textarea id="adresse" name="adresse" value="<?php echo((empty($_POST['adresse'])) ? "" : $_POST['adresse']); ?>-+" placeholder="Entrez votre adresse ici"></textarea>
+						    	</div>		   
+					   			<!--<input type="checkbox">Geolocalisation</input>-->
+					   		</div>
+				    		<div class="element">
+				    			<input type="submit" value="Go!"/>
+				    		</div>
+				    	</form>
+				    </div>
+					<div id="contenu"><!--
+						<? php
+							
+
+
+
+						 ?>-->
+						<p><a href="https://soutenir.msf.fr/b/mon-don?esv_source=Google&esv_medium=sea_brand&esv_campaign=W%2A%2AW00001&esv_term=msf&gclid=COKwx8uJvdMCFccp0wodkDMP4Q">
+								<div id="article 1" class="service">
+									Nom random 1 - 643m<br/>
+									<img src="static/accueil/msf_logo _fichiers/maison.jpeg" width=240 class="floatl" title="photo non contractuelle"/>
+										La cabane au fond de mon jardin<br/>DESCRIPTION.........................<br/>
+						...<br/>...............................
+						..............................................<br/><br/><br/>
+									Note:	4,4/5
+								</div>
+							</a>
+						</p>
+						<br/>
+						<p><a href="http://www.lecrystalparis.com/menu.html#mix">
+								<div id="article 2" class="service">
+										Nom random 2 - Paris I -729 m <br/>
+										<img src="static/accueil/msf_logo _fichiers/Worlds-Largest-Pizza.jpg" width=240 class=floatl title="photo non contractuelle" />
+										Repas à partager en famille<br/>.........................<br/>
 										...<br/>...............................
-										...........................................<br/><br/><br/></figcaption>
-								</figure>
-							</div>
-						</a>
-					</p>
-					<br/><br/><br/><br/><br/>
+										...........................................<br/><br/><br/>
+										Note:	4.8/5
+
+								</div>
+							</a>
+						</p>
+						<br/><br/><br/><br/><br/>
+					</div>
 				</div>
 				<div id="fil_actu">
 					<h3>Actu 1</h3><p>Blablabla<br/>BlablablaBlablablaBlablabla<br/>BlablablaBlablabla<br/></p>
 					<h3>Actu 2</h3><p>Blablabla<br/>BlablablaBlablablaBlablabla<br/>BlablablaBlablabla<br/></p>
 					<h3>Actu 3</h3><p>Blablabla<br/>BlablablaBlablablaBlablablabla<br/>BlablablaBlablabla<br/></p>
 				</div>
-			</section>	
+			</div>	
 				
 
 			<nav id="partage">
