404647e37011cfab0ea2c3d5b9515c2b35b8c70d
Hugo Briet <hugo.briet@isep.fr>
2017-05-22 22:37:46 +0200

    Update acceuil_admin.php

--- controllers/acceuil_admin.php
+++ controllers/acceuil_admin.php
@@ -1,11 +1,16 @@
 <?php
-	if(!empty($_POST['typeService'])){
+	if(!empty($_POST['typeService']) or !empty($_POST['nomService']) or !empty($_POST['arrondissement'])){
 		$typeService = $_POST['typeService'];
+		$nomService = $_POST['nomService'];
+		$arrondissement = $_POST['arrondissement'];
 		include("models/verificationService.php");
-		$data = dataTypeServices($typeService);
+		if(!empty($_POST['page'])){
+			$page = $_POST['page'];
+		}
+		else{
+			$page = 1;
+		}
+		$data = dataTypeService($typeService,$nomService,$arrondissement,$page);
 	}
-
 include("templates/acceuil_admin.php")
-
-
 ?>
