99b875d13320ec18bcc4ca942a646e983ac14fbf
hugobriet <hugo.briet@isep.fr>
2017-05-26 10:31:25 +0200

    correction code Antonin

--- controllers/servicesMaps.php
+++ controllers/servicesMaps.php
@@ -1,7 +1,13 @@
 <?php
 
 
-	
+
+	if(empty($_GET['adresse'])){
+		header("Location: ?page=locate");
+		exit();
+	}
+
+
 	$localisation=$_GET['adresse']; /*recupAdresse($bdd, $idService);*/
 	include("templates/servicesMaps.php");
 
