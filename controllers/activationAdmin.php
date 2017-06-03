36fcfaf48bda03971a9c65e98c6a2595ba2957bd
antoninmaillet <antonin.maillet@isep.fr>
2017-06-03 16:57:13 +0200

    dernier du we

--- controllers/activationAdmin.php
+++ controllers/activationAdmin.php
@@ -3,6 +3,7 @@
 // Récupération des variables nécessaires à l'activation
 $cle = $_GET['cle'];
 $email= $_GET['email'];
+$root = (!empty($_SERVER['HTTPS']) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . '/';
  
 // Récupération de la clé correspondant au $login dans la base de données
 
@@ -12,7 +13,12 @@ $inORnot=verifMailAdmin($bdd, $email);
 if($clebdda == $cle && $inORnot == true) // On compare nos deux clés	
        {
           // Si elles correspondent on active le compte !	
-          echo "Votre compte a bien été activé !";
+          echo "</br></br>";
+
+          echo "<h1>Félicitation ! Votre compte a bien été activé !</h1>";
+		  echo "<h1>Vous êtes maintenant administrateur</h1>";
+		  echo "<p>Quelques règles d'usages :</br>Vous ne devez pas accepter n'importe qui, vous représentez l'image de ce service d'aide. Vous ne devez en aucun cas transmettre des informations ou donner des privilèges.</p>";
+		  echo "<a href='".$root."'> Retour à l'accueil</a>";
  
           // La requête qui va passer notre champ actif de 0 à 1
           activeAdmin($bdd, $email);
