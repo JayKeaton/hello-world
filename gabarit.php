b7ca24a94799b48ee7bbf3b8fb0eadb570efaea7
antoninmaillet <antonin.maillet@isep.fr>
2017-06-08 11:29:56 +0200

    plan du site

--- gabarit.php
+++ gabarit.php
@@ -58,7 +58,7 @@
                         </div>
                     </a>
                 </li>
-                
+                <!--
                  <li>
                     <a href="<?php echo(SOUS_DOMAINE."?page=ajoutServices") ?>">
                         <div>
@@ -66,7 +66,7 @@
                             <p>Contributeur</p>
                         </div>
                     </a>
-                </li>
+                </li>-->
                 <?php
             }
             else {
@@ -95,14 +95,14 @@
                         </div>
                     </a>
                 </li>
-                <li>
+               <!-- <li>
                     <a href="http://www.partage.org/">
                         <div>
                             <img src="static/headerFooter/share.png" width=21/>
                             <p>Partage</p>
                         </div>
                     </a>
-                </li>
+                </li>-->
                 <li>
                     <a href="<?php echo(SOUS_DOMAINE."?page=profil") ?>">
                         <div>
@@ -119,14 +119,14 @@
                         </div>
                     </a>
                 </li>
-                <li>
+               <!-- <li>
                     <a href="https://fr.wikipedia.org/wiki/Param%C3%A8tre">
                         <div>
                             <img src="static/headerFooter/engrenages_1.png" width=21/>
                             <p>Paramètres</p>
                         </div>
                     </a>
-                </li>
+                </li>-->
                 <li>
                     <a href="<?php echo(SOUS_DOMAINE."?page=logout") ?>">
                         <div>
@@ -148,7 +148,7 @@
     
     <footer>
         <ul>
-            <li> <a href="http://hugobriet.000webhostapp.com/?page=accueil"> Plan du Site </a> </li>
+            <li> <a href=<?php echo(SOUS_DOMAINE."?page=planDuSite") ?>> Plan du Site </a> </li>
             <li> <a href=<?php echo(SOUS_DOMAINE."?page=contact") ?>> Contact </a> </li>
             <li> <a href="http://hugobriet.000webhostapp.com/?page=accueil"> En savoir plus </a> </li>
         </ul>
