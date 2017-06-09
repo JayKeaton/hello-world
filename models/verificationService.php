d1cc479ace12db59041138d4a3a189c32e0c6cfe
legyllith <jeremy.n.49@live.fr>
2017-06-09 17:37:14 +0200

    version final

--- models/verificationService.php
+++ models/verificationService.php
@@ -1,7 +1,8 @@
 <?php
-function dataTypeService ($page,$typeService){
+function dataTypeService ($typeService,$messagesParPage){
 	global $bdd;
 	$clause=array();
+	
 	
 	/*ici il s'agi de créer un tableau pour le remplir */
 	if(!empty($_POST['typeService'])) {
@@ -21,39 +22,12 @@ function dataTypeService ($page,$typeService){
 		$clause[]=$a;
 	}
 	$final = join(" AND ",$clause);
-	
-	$messagesParPage=5;
-	/* obtention du nombre de donné */
-	$retour_total=$bdd ->prepare("SELECT nom FROM descriptions JOIN services ON descriptions.idService = services.idService WHERE $final");
-	$retour_total->bindValue('typeService',$typeService);
-	$retour_total->execute();
-	$donnees_total=$retour_total-> fetchAll();
-	$total=count($donnees_total);
-	if(empty($_POST['page'])){
-		$page2=1;
-	}
-	else{
-		$page2=$_POST['page'];
-	}
-	/* obtention du nombre de page max */
-	$nombreDePages=ceil($total/$messagesParPage);
-	if(!empty($_POST['typeService']) or !empty($_POST['nomService']) or !empty($_POST['adresse']) or !empty($_POST['dejaValide'])){
-		$pageActuelle=$page2;
-		if($pageActuelle>$nombreDePages){
-			$pageActuelle=$nombreDePages;
-			/* petite sécurité*/
-			}
-		else{
-			$pageActuelle=1;
-		}
-		$offset=($pageActuelle-1)*$messagesParPage;
-	}
-	$req=$bdd -> prepare("SELECT nom,texte,validation FROM descriptions JOIN services ON descriptions.idService = services.idService WHERE $final
-	LIMIT $messagesParPage OFFSET $offset");
+	$req=$bdd -> prepare("SELECT nom,texte,validation,adresseImage FROM descriptions JOIN services ON descriptions.idService = services.idService WHERE $final");
 	$req->bindValue('typeService',$typeService);
 	$req->execute();
 	$data=$req-> fetchAll();
-	$retour= array($data,$nombreDePages,$pageActuelle);
+	$nombreDePages = ceil(count($data)/$messagesParPage);
+	$retour= array($data,$nombreDePages);
 	return $retour;
 }
 ?>
